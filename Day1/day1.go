fd63475fde6e7d7822f476fa7b9b05387c87df55
LucaCorra02 <luca.corradini4@gmail.com>
2023-12-01 11:02:57 +0100

    work on smallinput 2

--- Day1/day1.go
+++ Day1/day1.go
@@ -16,14 +16,48 @@ func elaboraInput() (tot int) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		match := regexp.MustCompile(`\d|one|two|three|four|five|six|seven|eight|nine`).FindAllString(scanner.Text(), -1)
-		num1, _ := strconv.Atoi(match[0])
+		convertMatch := convertStringNumtoInt(match)
+		num1 := convertMatch[0]
 		num1 *= 10
 		if len(match) == 1 {
 			tot += num1 + (num1 / 10)
 		} else {
-			num2, _ := strconv.Atoi(match[len(match)-1])
-			tot += num1 + num2
+			tot += num1 + convertMatch[len(convertMatch)-1]
 		}
+
 	}
 	return tot
+}
+
+func convertStringNumtoInt(match []string) (ris []int) {
+	for i := 0; i < len(match); i++ {
+		ris = append(ris, numStringToInt(match[i]))
+	}
+	return ris
+}
+
+func numStringToInt(str string) int {
+	switch str {
+	case "one":
+		return 1
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	default:
+		n, _ := strconv.Atoi(str)
+		return n
+	}
 }
