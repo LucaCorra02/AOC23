6a40de644b139118d2e6caa1455c2135c52ea70d
LucaCorra02 <luca.corradini4@gmail.com>
2023-12-01 10:38:20 +0100

    work on small input

--- Day1/day1.go
+++ Day1/day1.go
@@ -4,16 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	leggiFile()
-
+	fmt.Printf("il totale è : %d\n", elaboraInput())
 }
 
-func leggiFile() {
+func elaboraInput() (tot int) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		match := regexp.MustCompile(`\d`).FindAllString(scanner.Text(), -1)
+		num1, _ := strconv.Atoi(match[0])
+		num1 *= 10
+		if len(match) == 1 {
+			tot += num1 + (num1 / 10)
+		} else {
+			num2, _ := strconv.Atoi(match[len(match)-1])
+			tot += num1 + num2
+		}
 	}
+	return tot
 }
