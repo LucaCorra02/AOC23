96e334ee04d71da081b20c65c0e024e95ac0dfeb
LucaCorra02 <luca.corradini4@gmail.com>
2023-12-02 09:04:12 +0100

    add smallInput

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
 
+func main() {
+	leggiFile()
+
+}
+
+func leggiFile() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		match := regexp.MustCompile(`\d?=red`).FindAllString(line, -1)
+		fmt.Println(match)
+	}
 }
