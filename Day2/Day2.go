8fdd1c547098a4031e23cf3a1a5f23e04583ec02
LucaCorra02 <luca.corradini4@gmail.com>
2023-12-02 12:39:58 +0100

    refactor

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -6,37 +6,30 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	leggiFile()
-
 }
 
 func leggiFile() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var pt1, pt2, game int
 	for scanner.Scan() {
-		line := strings.Split(strings.TrimSpace(scanner.Text()), ";")
-		regex := regexp.MustCompile(`\d* r|\d* g|\d* b`)
-
+		match := regexp.MustCompile(`\d* r|\d* g|\d* b`).FindAllString(scanner.Text(), -1)
 		dict := map[byte]int{'r': 0, 'b': 0, 'g': 0}
-		for _, str := range line {
-			match := regex.FindAllString(str, -1)
-			for _, play := range match { //singole giocate nel match
-				color := len(play) - 1
-				n, _ := strconv.Atoi(play[:color-1])
-				dict[play[color]] = max(dict[play[color]], n)
-			}
+		for _, round := range match {
+			color := len(round) - 1
+			n, _ := strconv.Atoi(round[:color-1])
+			dict[round[color]] = max(dict[round[color]], n)
 		}
+
 		if dict['b'] <= 14 && dict['r'] <= 12 && dict['g'] <= 13 {
 			pt1 += game + 1
 		}
 		pt2 += dict['r'] * dict['g'] * dict['b']
 		game++
 	}
-
 	fmt.Printf("Parte 1 : %d, Parte 2 : %d\n", pt1, pt2)
 }
 
