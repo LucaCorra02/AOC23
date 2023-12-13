e03965df0e65e898bc32cb6b301658ce5af01ca8
LucaCorra02 <luca.corradini4@gmail.com>
2023-12-13 22:40:41 +0100

    add working pt2

--- Day9/day9.go
+++ Day9/day9.go
@@ -9,32 +9,44 @@ import (
 )
 
 func main() {
-	fmt.Printf("part 1: %d\n", leggiFile())
+	pt1, pt2 := leggiFile()
+	fmt.Printf("part1: %d, part2: %d\n", pt1, pt2)
 }
 
-func leggiFile() (tot int64) {
+func leggiFile() (tot1, tot2 int64) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		ranges := parsStringToInts(strings.Split(scanner.Text(), " "))
-		tot += calcRange(ranges)
+		n1, n2 := calcRange(ranges)
+		tot1 += n1
+		tot2 += n2
 	}
 	return
 }
 
-func calcRange(ranges []int64) (sum int64) {
+func calcRange(ranges []int64) (sum1, sum2 int64) {
 	var lastValue []int64
+	var firstValue []int64
 	for !checkAllZero(ranges) {
 		var newRanges []int64
 		for i := 0; i < len(ranges)-1; i++ {
 			diff := ranges[i+1] - ranges[i]
 			newRanges = append(newRanges, diff)
 		}
+		firstValue = append(firstValue, ranges[0])
 		lastValue = append(lastValue, ranges[len(ranges)-1])
 		ranges = newRanges
 	}
+
 	for _, val := range lastValue {
-		sum += val
+		sum1 += val
 	}
+
+	prev := firstValue[len(firstValue)-1]
+	for i := len(firstValue) - 1; i > 0; i-- {
+		prev = firstValue[i-1] - prev
+	}
+	sum2 = prev
 	return
 }
 
